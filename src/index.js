8df4fc529c260238726778077254ece30e555c08
Stefan Ingthorsson <ingthorsson@gmail.com>
2022-02-10 23:49:31 +0000

    test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const App = (props) => {
+  const [ count, setCount ] = useState(props.count)
+  const [ text, setText ] = useState('')
+
+  return (
+     <div>
+       <p> The current {text || 'count' } is {count}</p>
+       <button onClick={() => setCount(count + 1)}>+1</button>
+       <button onClick={() => setCount(count - 1)}>-1</button>
+       <button onClick={() => setCount(props.count)}>Reset</button>
+       <input value = {text} onChange={(e) => setText(e.target.value) }/>
+     </div>
+  )
+}
+
+App.defaultProps = {
+  count: 9
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App count={2} />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
